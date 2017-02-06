cb300d12df5c0ed8fe8f072ad2123a19b1663fe4
Fabio Tiriticco <fabio.europe@gmail.com>
2017-02-06 21:50:21 +0100

    comments

--- src/main/scala/helloworld/HelloWorld.scala
+++ src/main/scala/helloworld/HelloWorld.scala
@@ -2,17 +2,29 @@ package helloworld
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
 /**
   * ActorDemo
   * Created by fabiotiriticco on 06/02/2017.
   */
 object HelloWorld extends App {
+
+  // instantiate actor system
   val as = ActorSystem()
+
+  // instantiate actor
   val pingActor = as.actorOf(Props(new PingActor))
 
+  // send messages to actor
   pingActor ! Ping
   pingActor ! Ping
   pingActor ! Ping
+
+  // terminate the actor system
+  as.terminate()
+  Await.result(as.whenTerminated, Duration.Inf)
 }
 
 case object Ping
