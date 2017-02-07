5973178e8f06c2caace0ce7e1f30c79e317e1156
Fabio Tiriticco <fabio.europe@gmail.com>
2017-02-07 14:57:53 +0100

    comments

--- src/main/scala/helloworld/HelloWorld.scala
+++ src/main/scala/helloworld/HelloWorld.scala
@@ -5,38 +5,51 @@ import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-/**
-  * ActorDemo
-  * Created by fabiotiriticco on 06/02/2017.
-  */
-object HelloWorld extends App {
-
-  // instantiate actor system
-  val as = ActorSystem()
-
-  // instantiate actor
-  val pingActor = as.actorOf(Props(new PingActor))
-
-  // send messages to actor
-  pingActor ! Ping
-  pingActor ! Ping
-  pingActor ! Ping
-
-  // terminate the actor system
-  as.terminate()
-  Await.result(as.whenTerminated, Duration.Inf)
-}
-
+// a ping (no information attached)
 case object Ping
 
-case object Pong
+// a pong class (carries information)
+case class Pong(pings: Int)
 
+// the ping actor
 class PingActor extends Actor with ActorLogging {
+
+  // internal state
   var pings = 0
 
   override def receive = {
     case Ping =>
       pings = pings + 1
-      log.info("received {} Ping", pings)
+      sender ! Pong(pings)
   }
+}
+
+// the supervisor
+class Supervisor extends Actor with ActorLogging {
+
+  // instantiate ping actor
+  val pingActor = context.actorOf(Props(new PingActor))
+
+  // send messages to ping actor
+  pingActor ! Ping
+  pingActor ! Ping
+  pingActor ! Ping
+
+  override def receive = {
+    case Pong(pings) => log.info("received Pong with {}", pings)
+  }
+}
+
+// the program entry point
+object HelloWorld extends App {
+
+  // instantiate actor system
+  val as = ActorSystem()
+
+  // instantiate supervisor
+  as.actorOf(Props(new Supervisor), "supervisor")
+
+  // terminate the actor system
+  as.terminate()
+  Await.result(as.whenTerminated, Duration.Inf)
 }
